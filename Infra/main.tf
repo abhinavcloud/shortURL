c7fa8f5b89ae27e4b3a4379aa569a11c59dca86d
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-04-28 12:12:26 +0530

    update

--- Infra/main.tf
+++ Infra/main.tf
@@ -6,7 +6,4 @@ provider "aws" {
 
 module "Storage" {
   source          = "../Storage/"
-  common_tags     = local.common_tags
-  vpc_endpoint_id = module.Network.vpc_endpoint_id
-  depends_on      = [module.Security_Groups]
 }
