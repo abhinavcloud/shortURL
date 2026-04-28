8656b853fe2f91b6ddf19ad488e85a8261ac1a41
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-04-28 16:51:50 +0530

    Added cloudfront resources and variable

---
+++ Infra/Cloudfront/variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" {
+  description = "S3 bucket name for the website"
+  type        = string
+}
+
+variable "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  type        = string
+}
+
+variable "bucket_regional_domain_name" {
+    description = "Regional domain name"
+    type = string
+}
