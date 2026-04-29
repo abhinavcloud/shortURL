10a12183fadfebc4e4842870a7e169f5a22b0302
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-04-29 18:11:34 +0530

    invalidating cloudfront cache

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -3,6 +3,11 @@ output "cloudfront_domain_name" {
   description = "CloudFront domain name"
 }
 
+output "distribution_id" {
+  value       = module.Cloudfront.distribution_id
+  description = "CloudFront distribution id (use in CI for invalidations)"
+}
+
 output "cognito_domain" {
   value = module.Authentication.cognito_domain
 }
