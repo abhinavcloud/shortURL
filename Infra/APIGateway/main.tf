84a9d711db6cb605d5e5ac03a49d86edb4dbcb38
Abhinav Kumar <abhinav@abhinav-cloud.com>
2026-05-02 23:33:13 +0530

    completed all code base for v1

--- Infra/APIGateway/main.tf
+++ Infra/APIGateway/main.tf
@@ -3,7 +3,7 @@ resource "aws_apigatewayv2_api" "shorturl" {
   protocol_type = "HTTP"
   
   cors_configuration {
-    allow_origins = var.allowed_origins
+    allow_origins = ["*"]
     allow_methods = ["OPTIONS", "POST", "GET"]
     allow_headers = ["content-type", "authorization"]
     max_age       = 86400
